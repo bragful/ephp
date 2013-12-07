6fc12eb0a15c3ef44c7ba47b357bf62f2ec454ad
Manuel Rubio <manuel@bosqueviejo.net>
2013-12-07 03:20:47 +0100

    add tests for context

---
+++ apps/ephp/test/ephp_context_test.erl
@@ -0,0 +1,44 @@
+-module(ephp_context_test).
+-compile(export_all).
+
+-include_lib("eunit/include/eunit.hrl").
+
+create_and_stop_test() ->
+    {ok, Ctx} = ephp_context:start_link(),
+    ?assert(is_process_alive(Ctx)),
+    ephp_context:destroy(Ctx),
+    timer:sleep(500), 
+    ?assertNot(is_process_alive(Ctx)).
+
+setup_test_() ->
+    {foreach,
+        fun start/0,
+        fun stop/1, [
+        fun set_and_get_test/1,
+        fun set_and_get_array_test/1
+    ]}.
+
+start() ->
+    {ok, Ctx} = ephp_context:start_link(),
+    Ctx.
+
+stop(Ctx) ->
+    ephp_context:destroy(Ctx). 
+
+set_and_get_test(Ctx) -> [
+    ?_assertEqual(ok, ephp_context:set(Ctx,{var,<<"a">>,[]},10)),
+    ?_assertEqual(ok, ephp_context:set(Ctx,{var,<<"b">>,[]},20)),
+    ?_assertEqual(10, ephp_context:get(Ctx,{var,<<"a">>,[]})),
+    ?_assertEqual(20, ephp_context:get(Ctx,{var,<<"b">>,[]}))
+].
+
+set_and_get_array_test(Ctx) -> [
+    ?_assertEqual(ok, ephp_context:set(Ctx,{var,<<"a">>,[{int,0}]},10)),
+    ?_assertEqual(ok, ephp_context:set(Ctx,{var,<<"a">>,[{int,1}]},20)),
+    ?_assertEqual(ok, ephp_context:set(Ctx,{var,<<"a">>,[{text,<<"hola">>}]},30)),
+    ?_assertEqual(ok, ephp_context:set(Ctx,{var,<<"a">>,[{int,2},{int,0}]},40)),
+    ?_assertEqual(10, ephp_context:get(Ctx,{var,<<"a">>,[{int,0}]})),
+    ?_assertEqual(20, ephp_context:get(Ctx,{var,<<"a">>,[{int,1}]})),
+    ?_assertEqual(30, ephp_context:get(Ctx,{var,<<"a">>,[{text,<<"hola">>}]})),
+    ?_assertEqual(40, ephp_context:get(Ctx,{var,<<"a">>,[{int,2},{int,0}]}))
+].
