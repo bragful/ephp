35b4cce8dbf4c3347c9e8410ffd0a3360ac27e6f
Manuel Rubio <manuel@bosqueviejo.net>
2013-12-14 02:56:46 +0100

    add tests for context

--- test/ephp_context_test.erl
+++ test/ephp_context_test.erl
@@ -2,6 +2,7 @@
 -compile(export_all).
 
 -include_lib("eunit/include/eunit.hrl").
+-include("ephp.hrl").
 
 create_and_stop_test() ->
     {ok, Ctx} = ephp_context:start_link(),
@@ -15,7 +16,10 @@ setup_test_() ->
         fun start/0,
         fun stop/1, [
         fun set_and_get_test/1,
-        fun set_and_get_array_test/1
+        fun set_and_get_array_test/1,
+        fun resolve_assign/1,
+        fun operation_test/1,
+        fun arith_mono_test/1
     ]}.
 
 start() ->
@@ -28,8 +32,15 @@ stop(Ctx) ->
 set_and_get_test(Ctx) -> [
     ?_assertEqual(ok, ephp_context:set(Ctx,{variable,<<"a">>,[]},10)),
     ?_assertEqual(ok, ephp_context:set(Ctx,{variable,<<"b">>,[]},20)),
+    ?_assertEqual(ok, ephp_context:set(Ctx,{variable,<<"c">>,[]},undefined)),
+    ?_assertEqual(ok, ephp_context:set(Ctx,{variable,<<"d">>,[]},true)),
+    ?_assertEqual(ok, ephp_context:set(Ctx,{variable,<<"e">>,[]},false)),
+
     ?_assertEqual(10, ephp_context:get(Ctx,{variable,<<"a">>,[]})),
-    ?_assertEqual(20, ephp_context:get(Ctx,{variable,<<"b">>,[]}))
+    ?_assertEqual(20, ephp_context:get(Ctx,{variable,<<"b">>,[]})),
+    ?_assertEqual(undefined, ephp_context:get(Ctx,{variable,<<"c">>,[]})),
+    ?_assertEqual(true, ephp_context:get(Ctx,{variable,<<"d">>,[]})),
+    ?_assertEqual(false, ephp_context:get(Ctx,{variable,<<"e">>,[]}))
 ].
 
 set_and_get_array_test(Ctx) -> [
@@ -41,4 +52,27 @@ set_and_get_array_test(Ctx) -> [
     ?_assertEqual(20, ephp_context:get(Ctx,{variable,<<"a">>,[{int,1}]})),
     ?_assertEqual(30, ephp_context:get(Ctx,{variable,<<"a">>,[{text,<<"hola">>}]})),
     ?_assertEqual(40, ephp_context:get(Ctx,{variable,<<"a">>,[{int,2},{int,0}]}))
+].
+
+resolve_assign(Ctx) -> [
+    ?_assertEqual(10, ephp_context:solve(Ctx,#assign{variable=#variable{name = <<"a">>}, expression=#int{int=10}}))
+].
+
+operation_test(Ctx) -> [
+    ?_assertEqual(10, ephp_context:solve(Ctx, #operation{type = <<"+">>, expression_left=#int{int=5}, expression_right=#int{int=5}})),
+    ?_assertEqual(0, ephp_context:solve(Ctx, #operation{type = <<"-">>, expression_left=#int{int=5}, expression_right=#int{int=5}})),
+    ?_assertEqual(25, ephp_context:solve(Ctx, #operation{type = <<"*">>, expression_left=#int{int=5}, expression_right=#int{int=5}})),
+    ?_assertEqual(1.0, ephp_context:solve(Ctx, #operation{type = <<"/">>, expression_left=#int{int=5}, expression_right=#int{int=5}})),
+    ?_assertEqual(true, ephp_context:solve(Ctx, #operation{type = <<"<">>, expression_left=#int{int=1}, expression_right=#int{int=5}})),
+    ?_assertEqual(false, ephp_context:solve(Ctx, #operation{type = <<">">>, expression_left=#int{int=1}, expression_right=#int{int=5}}))
+].
+
+arith_mono_test(Ctx) ->
+    Var = {variable,<<"a">>,[]}, [
+    ?_assertEqual(ok, ephp_context:set(Ctx,Var,10)),
+    ?_assertEqual(11, ephp_context:solve(Ctx, {pre_incr, Var})),
+    ?_assertEqual(10, ephp_context:solve(Ctx, {pre_decr, Var})),
+    ?_assertEqual(10, ephp_context:solve(Ctx, {post_incr, Var})),
+    ?_assertEqual(11, ephp_context:solve(Ctx, {post_decr, Var})),
+    ?_assertEqual(10, ephp_context:get(Ctx,Var))
 ].
