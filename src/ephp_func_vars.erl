5650228f333537ff149dfde3a538ba3cf45c679a
Manuel Rubio <manuel@bosqueviejo.net>
2013-12-17 03:21:18 +0100

    add vars handle functions

---
+++ src/ephp_func_vars.erl
@@ -0,0 +1,63 @@
+-module(ephp_func_vars).
+-compile([warnings_as_errors]).
+
+-export([
+    init/1,
+    is_bool/2,
+    print_r/2
+]).
+
+-include("ephp.hrl").
+
+-spec init(Context :: context()) -> ok.
+
+init(Context) ->
+    Funcs = [
+        is_bool, print_r
+    ],
+    lists:foreach(fun(Func) ->
+        Name = atom_to_binary(Func, utf8),
+        ephp_context:register_func(Context, Name, ?MODULE, Func)  
+    end, Funcs), 
+    ok. 
+
+-spec is_bool(Context :: context(), Value :: mixed()) -> boolean().
+
+is_bool(_Context, Value) when is_boolean(Value) -> 
+    true;
+
+is_bool(_Context, _Value) -> 
+    false.
+
+-spec print_r(Context :: context(), Value :: mixed()) -> boolean().
+
+print_r(_Context, Value) when not ?IS_DICT(Value) -> 
+    ephp_util:to_bin(Value);
+
+print_r(_Context, Value) ->
+    Data = lists:foldl(fun(Chunk,Total) ->
+        <<Total/binary, Chunk/binary>>
+    end, <<>>, print_r_fmt(Value, <<"    ">>)),
+    <<"Array\n(\n", Data/binary, ")\n">>.
+
+print_r_fmt(Value, _Spaces) when not ?IS_DICT(Value) -> 
+    <<(ephp_util:to_bin(Value))/binary, "\n">>;
+
+print_r_fmt(Value, Spaces) ->
+    ?DICT:fold(fun(Key, Val, Res) ->
+        KeyBin = ephp_util:to_bin(Key),
+        Res ++ case print_r_fmt(Val, <<Spaces/binary, "    ">>) of
+            V when is_binary(V) -> 
+                [<<Spaces/binary, "[", KeyBin/binary, "] => ", V/binary>>];
+            V when is_list(V) ->
+                Content = lists:map(fun(Element) ->
+                    <<Spaces/binary, Element/binary>>
+                end, V),
+                [
+                    <<Spaces/binary, "[", KeyBin/binary, "] => Array\n">>, 
+                    <<Spaces/binary, "(\n">>
+                ] ++ Content ++ [
+                    <<Spaces/binary, ")\n">>
+                ]
+        end
+    end, [], Value).
