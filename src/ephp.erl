2478cfce196ff5855a828d7dc06db057831d3f1f
Manuel Rubio <manuel@bosqueviejo.net>
2013-12-17 20:02:05 +0100

    add run and compile to the interface for compile once and run at any time

--- src/ephp.erl
+++ src/ephp.erl
@@ -6,6 +6,8 @@
     register_var/3,
     register_fun/4,
     register_module/2,
+    compile/1,
+    run/2,
     eval/2,
 
     main/1   %% for escriptize
@@ -57,6 +59,24 @@ register_fun(Ctx, PHPName, Module, Fun) ->
 
 register_module(Ctx, Module) ->
     Module:init(Ctx).
+
+-spec compile(PHP :: binary() | string()) -> 
+    {ok, Result :: [statement()]} | {error, Reason :: reason()} | 
+    {error,{Code::binary(), Line::integer(), Col::integer()}}.
+
+compile(PHP) ->
+    case ephp_parser:parse(PHP) of
+        {_,Code,{{line,Line},{column,Col}}} ->
+            {error, {Code,Line,Col}};
+        Compiled ->
+            {ok, Compiled}
+    end.
+
+-spec run(Context :: context(), Compiled :: [statement()]) -> 
+    {ok, binary()} | {error, Reason::reason()}.
+
+run(Context, Compiled) ->
+    ephp_interpr:process(Context, Compiled).
 
 -spec eval(Context :: context(), PHP :: string() | binary()) -> 
     {ok, Result :: binary()} | {error, Reason :: reason()} | 
