1e4e6267ed7bf3ee3efabb446e557b9a9d149c6a
Manuel Rubio <manuel@bosqueviejo.net>
2014-03-13 01:28:30 +0200

    define a flush handler as stdout, file descriptor or closure

--- src/ephp_output.erl
+++ src/ephp_output.erl
@@ -6,8 +6,13 @@
 
 -include("ephp.hrl").
 
+-type flush_handler() ::
+    stdout | {io, io:device()} | function().
+
 -record(state, {
-    output = <<>> :: binary()
+    output = <<>> :: binary(),
+    flush = true :: boolean(),
+    flush_handler = stdout :: flush_handler()
 }).
 
 %% ------------------------------------------------------------------
@@ -16,8 +21,11 @@
 
 -export([
     start_link/0,
+    start_link/1,
+    start_link/2,
     get/1,
-    set/2
+    set/2,
+    destroy/1
 ]).
 
 %% ------------------------------------------------------------------
@@ -32,20 +40,32 @@
 %% ------------------------------------------------------------------
 
 start_link() ->
-    gen_server:start_link(?MODULE, [], []).
+    start_link(true, stdout).
 
-get(Context) ->
-    gen_server:call(Context, output).
+start_link(Flush) ->
+    start_link(Flush, stdout).
 
-set(Context, Text) ->
-    gen_server:cast(Context, {output, Text}).
+start_link(Flush, FlushHandler) ->
+    gen_server:start_link(?MODULE, [Flush, FlushHandler], []).
+
+get(Output) ->
+    gen_server:call(Output, output).
+
+set(Output, Text) ->
+    gen_server:cast(Output, {output, Text}).
+
+destroy(Output) ->
+    gen_server:cast(Output, stop).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
-init([]) ->
-    {ok, #state{}}.
+init([Flush, FlushHandler]) ->
+    {ok, #state{
+        flush = Flush,
+        flush_handler = FlushHandler
+    }}.
 
 handle_call(output, _From, #state{output=Output}=State) ->
     {reply, Output, State#state{output = <<>>}};
@@ -57,7 +77,11 @@ handle_call(_Request, _From, State) ->
 handle_cast(stop, State) ->
     {stop, normal, State};
 
-handle_cast({output, Text}, #state{output=Output}=State) ->
+handle_cast({output, Text}, #state{flush=true, flush_handler=FH}=State) ->
+    flush_handler(Text, FH),
+    {noreply, State};
+
+handle_cast({output, Text}, #state{flush=false, output=Output}=State) ->
     NewState = State#state{output = <<Output/binary, Text/binary>>},
     {noreply, NewState};
 
@@ -68,6 +92,9 @@ handle_cast(_Msg, State) ->
 handle_info(_Info, State) ->
     {noreply, State}.
 
+terminate(_Reason, #state{flush=false, flush_handler=FH, output=Output}) ->
+    flush_handler(Output, FH),
+    ok;
 
 terminate(_Reason, _State) ->
     ok.
@@ -79,3 +106,15 @@ code_change(_OldVsn, State, _Extra) ->
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
+
+flush_handler(Text, stdout) ->
+    io:fwrite("~s", [Text]),
+    ok;
+
+flush_handler(Text, {io, FH}) ->
+    io:fwrite(FH, "~s", [Text]),
+    ok;
+
+flush_handler(Text, FH) when is_function(FH) ->
+    FH(Text),
+    ok.
