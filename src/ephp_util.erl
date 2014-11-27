b8c5da9d6ea832e9ef0eb9987f459a1769c7b181
Manuel Rubio <manuel@bosqueviejo.net>
2014-11-28 00:37:04 +0200

    remove deprecation message about the use of dict() for 17.X+

--- src/ephp_util.erl
+++ src/ephp_util.erl
@@ -71,7 +71,7 @@ increment_code(Code) when is_binary(Code) ->
     end.
 
 -spec to_bool(Value :: undefined |
-    dict() | integer() | float() | string() | binary()) -> boolean().
+    dict:dict() | integer() | float() | string() | binary()) -> boolean().
 
 to_bool(undefined) -> false;
 
@@ -87,7 +87,7 @@ to_bool(_Other) -> true.
 
 
 -spec zero_if_undef(Value :: undefined | 
-    dict() | integer() | float() | string() | binary()) -> integer().
+    dict:dict() | integer() | float() | string() | binary()) -> integer().
 
 zero_if_undef(undefined) -> 0;
 
