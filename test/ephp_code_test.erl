419863792fd509c4fabb8bd5ab7b36da631eaae9
Manuel Rubio <manuel@altenwald.com>
2014-12-03 19:59:45 +0200

    add tests in PHP format with output check

---
+++ test/ephp_code_test.erl
@@ -0,0 +1,29 @@
+-module(ephp_code_test).
+-compile(export_all).
+
+-define(CODE_PATH, "../test/code/").
+
+-include_lib("eunit/include/eunit.hrl").
+
+eval(Filename) ->
+    case file:read_file(Filename) of
+    {ok, Content} ->
+        AbsFilename = list_to_binary(filename:absname(Filename)),
+        {ok, Ctx} = ephp:context_new(AbsFilename),
+        {ok, Output} = ephp_output:start_link(false),
+        ephp_context:set_output_handler(Ctx, Output),
+        ephp:eval(AbsFilename, Ctx, Content);
+    Error ->
+        Error
+    end.
+
+test_code(File) ->
+    {ok, OutCode} = eval(?CODE_PATH ++ File ++ ".php"),
+    {ok, OutFile} = file:read_file(?CODE_PATH ++ File ++ ".out"),
+    ?_assertEqual(OutCode, OutFile).
+
+code_to_test_() ->
+    Codes = [
+        "text_op"
+    ],
+    lists:map(fun(X) -> test_code(X) end, Codes).
