d8588fdf8291dc42c24181f473ef8b23ccad479d
Manuel Rubio <manuel@altenwald.com>
2015-06-17 23:06:30 +0200

    reordering the chaos

--- src/ephp_interpr.erl
+++ src/ephp_interpr.erl
@@ -28,8 +28,10 @@ process(Context, Statements) ->
     end, false, Statements),
     {ok, Value}.
 
+-type flow_status() :: break | continue | return() | false.
+
 -spec run(Context :: context(), Statements :: main_statement()) ->
-    break | continue | return() | false.
+    flow_status().
 
 run(Context, #print_text{text=Text}) ->
     ephp_context:set_output(Context, Text),
@@ -41,132 +43,162 @@ run(Context, #print{expression=Expr}) ->
     false;
 
 run(Context, #eval{statements=Statements}) ->
+    lists:foldl(fun(Statement, State) ->
+        run_depth(Context, Statement, State)
+    end, false, Statements).
+
+-spec run_depth(context(), statement(), flow_status()) ->
+    flow_status().
+
+
+run_depth(Context, #eval{}=Eval, false) ->
+    run(Context, Eval);
+
+run_depth(Context, #assign{}=Assign, Return) ->
+    ephp_context:solve(Context, Assign),
+    Return;
+
+run_depth(Context, #if_block{conditions=Cond}=IfBlock, false) ->
+    case ephp_context:solve(Context, Cond) of
+    true ->
+        Break = run(Context, 
+            #eval{statements=IfBlock#if_block.true_block}),
+        Break;
+    false when IfBlock#if_block.false_block =/= undefined ->
+        Break = run(Context, 
+            #eval{statements=IfBlock#if_block.false_block}),
+        Break;
+    _ ->
+        false
+    end;
+
+run_depth(Context, #switch{condition=Cond, cases=Cases}, false) ->
     lists:foldl(fun
-        (#eval{}=Eval, false) ->
-            run(Context, Eval);
-        (#assign{}=Assign, Return) ->
-            ephp_context:solve(Context, Assign),
-            Return;
-        (#if_block{conditions=Cond}=IfBlock, false) ->
-            case ephp_context:solve(Context, Cond) of
+        (_SwitchCase, false) ->
+            false;
+        (#switch_case{label=default, code_block=Code}, _Flow) ->
+            run(Context, #eval{statements=Code}),
+            break;
+        (#switch_case{label=LabelValue}=Case, Flow) ->
+            MatchValue = ephp_context:solve(Context, Cond),
+            Op = #operation{
+                type = <<"==">>,
+                expression_left=MatchValue,
+                expression_right=LabelValue},
+            case ephp_context:solve(Context, Op) orelse Flow =:= true of
             true ->
                 Break = run(Context, 
-                    #eval{statements=IfBlock#if_block.true_block}),
-                Break;
-            false when IfBlock#if_block.false_block =/= undefined ->
-                Break = run(Context, 
-                    #eval{statements=IfBlock#if_block.false_block}),
-                Break;
-            _ ->
-                false
-            end;
-        (#switch{condition=Cond, cases=Cases}, false) ->
-            lists:foldl(fun
-                (#switch_case{label=default, code_block=Code}, Flow)
-                        when Flow =/= false ->
-                    run(Context, #eval{statements=Code}),
-                    break;
-                (#switch_case{label=LabelValue}=Case, Flow)
-                        when Flow =/= false ->
-                    MatchValue = ephp_context:solve(Context, Cond),
-                    Op = #operation{
-                        type = <<"==">>,
-                        expression_left=MatchValue,
-                        expression_right=LabelValue},
-                    case ephp_context:solve(Context, Op) orelse Flow =/= match of
-                    true ->
-                        Break = run(Context, 
-                            #eval{statements=Case#switch_case.code_block}),
-                        Break =/= break;
-                    false ->
-                        match
-                    end;
-                (_SwitchCase, false) ->
-                    false
-            end, match, Cases),
-            false;
-        (#for{init=Init,conditions=Cond,
-                update=Update,loop_block=LB}, false) ->
-            run(Context, #eval{statements=Init}),
-            LoopBlock = if
-                is_tuple(LB) -> [LB];
-                is_list(LB) -> LB;
-                is_atom(LB) -> [LB];
-                LB =:= undefined -> []
-            end,
-            run_loop(pre, Context, Cond, LoopBlock ++ Update);
-        (#foreach{kiter=Key,iter=Var,elements=RawElements,loop_block=LB}, false) ->
-            LoopBlock = if
-                is_tuple(LB) -> [LB];
-                is_list(LB) -> LB;
-                is_atom(LB) -> [LB];
-                LB =:= undefined -> []
-            end,
-            Elements = ?DICT:to_list(ephp_context:get(Context, RawElements)),
-            run_foreach(Context, Key,Var,Elements,LoopBlock);
-        (#while{type=Type,conditions=Cond,loop_block=LB}, false) ->
-            LoopBlock = if
-                is_tuple(LB) -> [LB];
-                is_list(LB) -> LB;
-                is_atom(LB) -> [LB];
-                LB =:= undefined -> []
-            end, 
-            run_loop(Type, Context, Cond, LoopBlock);
-        (#print_text{text=Text}, false) ->
-            ephp_context:set_output(Context, Text),
-            false;
-        (#print{expression=Expr}, false) ->
-            Result = ephp_context:solve(Context, Expr),
-            ResText = ephp_util:to_bin(Result),
-            ephp_context:set_output(Context, ResText),
-            false;
-        (#call{}=Call, false) ->
-            ephp_func:run(Context, Call);
-        ({Op, _Var, _Line}=MonoArith, false) when 
-                Op =:= pre_incr orelse 
-                Op =:= pre_decr orelse
-                Op =:= post_incr orelse
-                Op =:= post_decr ->
-            ephp_context:solve(Context, MonoArith),
-            false;
-        (#operation{}=Op, false) ->
-            ephp_context:solve(Context, Op),
-            false;
-        (#class{}=Class, Return) ->
-            ephp_context:register_class(Context, Class),
-            Return;
-        (#function{name=Name, args=Args, code=Code}, Return) ->
-            ephp_context:register_func(Context, Name, Args, Code),
-            Return;
-        ({global, GlobalVar, Line}, Return) ->
-            ephp_context:solve(Context, {global, GlobalVar, Line}),
-            Return;
-        (break, false) ->
-            break;
-        (continue, false) ->
-            continue;
-        ({return,Value,_Line}, false) ->
-            {return, ephp_context:solve(Context, Value)};
-        ({return,Value}, false) ->
-            {return, Value};
-        (#int{}, false) ->
-            false;
-        (#float{}, false) ->
-            false;
-        (#text{}, false) ->
-            false;
-        (#text_to_process{}=TP, false) ->
-            ephp_context:solve(Context, TP),
-            false;
-        (#variable{idx=[{object,#call{},_}]}=Var, false) ->
-            ephp_context:solve(Context, Var);
-        (_Statement, false) ->
-            %% TODO: do better error handling
-            io:format("FATAL: ~p~n", [_Statement]),
-            throw(eunknownst);
-        (_Statement, Break) ->
-            Break
-    end, false, Statements).
+                    #eval{statements=Case#switch_case.code_block}),
+                Break =/= break;
+            false ->
+                seek
+            end
+    end, seek, Cases),
+    false;
+
+run_depth(Context, #for{init=Init,conditions=Cond,
+        update=Update,loop_block=LB}, false) ->
+    run(Context, #eval{statements=Init}),
+    LoopBlock = if
+        is_tuple(LB) -> [LB];
+        is_list(LB) -> LB;
+        is_atom(LB) -> [LB];
+        LB =:= undefined -> []
+    end,
+    run_loop(pre, Context, Cond, LoopBlock ++ Update);
+
+run_depth(Context, #foreach{
+        kiter=Key, iter=Var, elements=RawElements, loop_block=LB}, false) ->
+    LoopBlock = if
+        is_tuple(LB) -> [LB];
+        is_list(LB) -> LB;
+        is_atom(LB) -> [LB];
+        LB =:= undefined -> []
+    end,
+    Elements = ?DICT:to_list(ephp_context:get(Context, RawElements)),
+    run_foreach(Context, Key,Var,Elements,LoopBlock);
+
+run_depth(Context, #while{type=Type,conditions=Cond,loop_block=LB}, false) ->
+    LoopBlock = if
+        is_tuple(LB) -> [LB];
+        is_list(LB) -> LB;
+        is_atom(LB) -> [LB];
+        LB =:= undefined -> []
+    end, 
+    run_loop(Type, Context, Cond, LoopBlock);
+
+run_depth(Context, #print_text{text=Text}, false) ->
+    ephp_context:set_output(Context, Text),
+    false;
+
+run_depth(Context, #print{expression=Expr}, false) ->
+    Result = ephp_context:solve(Context, Expr),
+    ResText = ephp_util:to_bin(Result),
+    ephp_context:set_output(Context, ResText),
+    false;
+
+run_depth(Context, #call{}=Call, false) ->
+    ephp_func:run(Context, Call);
+
+run_depth(Context, {Op, _Var, _Line}=MonoArith, false) when 
+        Op =:= pre_incr orelse 
+        Op =:= pre_decr orelse
+        Op =:= post_incr orelse
+        Op =:= post_decr ->
+    ephp_context:solve(Context, MonoArith),
+    false;
+
+run_depth(Context, #operation{}=Op, false) ->
+    ephp_context:solve(Context, Op),
+    false;
+
+run_depth(Context, #class{}=Class, Return) ->
+    ephp_context:register_class(Context, Class),
+    Return;
+
+run_depth(Context, #function{name=Name, args=Args, code=Code}, Return) ->
+    ephp_context:register_func(Context, Name, Args, Code),
+    Return;
+
+run_depth(Context, {global, GlobalVar, Line}, Return) ->
+    ephp_context:solve(Context, {global, GlobalVar, Line}),
+    Return;
+
+run_depth(_Context, break, false) ->
+    break;
+
+run_depth(_Context, continue, false) ->
+    continue;
+
+run_depth(Context, {return,Value,_Line}, false) ->
+    {return, ephp_context:solve(Context, Value)};
+
+run_depth(_Context, {return,Value}, false) ->
+    {return, Value};
+
+run_depth(_Context, #int{}, false) ->
+    false;
+
+run_depth(_Context, #float{}, false) ->
+    false;
+
+run_depth(_Context, #text{}, false) ->
+    false;
+
+run_depth(Context, #text_to_process{}=TP, false) ->
+    ephp_context:solve(Context, TP),
+    false;
+
+run_depth(Context, #variable{idx=[{object,#call{},_}]}=Var, false) ->
+    ephp_context:solve(Context, Var);
+
+run_depth(_Context, _Statement, false) ->
+    %% TODO: do better error handling
+    io:format("FATAL: ~p~n", [_Statement]),
+    throw(eunknownst);
+
+run_depth(_Context, _Statement, Break) ->
+    Break.
 
 -spec run_loop(
     PrePost :: (pre | post),
