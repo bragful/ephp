5d17d8e1d373d57f428408f09007f3bfcffb89c8
Manuel Rubio <manuel@altenwald.com>
2016-09-24 05:08:07 +0100

    add tests for math library

---
+++ test/code/test_math.php
@@ -0,0 +1,22 @@
+<?php
+
+print "sqrt(3) = " . sqrt(3) . "\n";
+print "sqrt('11.1a') = " . sqrt('a') . "\n";
+print "ceil(sqrt(3)) = " . ceil(sqrt(3)) . "\n";
+print "ceil(-10.5) = " . ceil(-10.5) . "\n";
+print "ceil('10.5a') = " . ceil('a') . "\n";
+print "floor(sqrt(3)) = " . floor(sqrt(3)) . "\n";
+print "floor(-10.5) = " . floor(-10.5) . "\n";
+print "floor(-10) = " . floor(-10) . "\n";
+print "floor('45.3a') = " . floor('a') . "\n";
+print "round(sqrt(3)) = " . round(sqrt(3)) . "\n";
+print "round(-10.5) = " . round(-10.5) . "\n";
+print "round('5.5a') = " . round('a') . "\n";
+print "abs(10) = " . abs(10) . "\n";
+print "abs(-10) = " . abs(-10) . "\n";
+print "abs('a') = " . abs('a') . "\n";
+print "abs([]) = " . print_r(abs([]), true) . "\n";
+print "acos(1.0) = " . acos(1.0) . "\n";
+print "acos('a') = " . acos('a') . "\n";
+print "asin(1.0) = " . asin(1.0) . "\n";
+print "asin('a') = " . asin('a') . "\n";
