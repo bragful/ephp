85f381e2e40290ad166f8cfc7e0df17b8f31f472
Manuel Rubio <manuel@altenwald.com>
2016-10-03 01:18:14 +0100

    fix tests for halt

--- test/ephp_test.erl
+++ test/ephp_test.erl
@@ -8,16 +8,16 @@ main_test() ->
     ?assertEqual(0, ephp:main(["test/code/test_empty.php"])).
 
 main_file_not_found_test() ->
-    ?assertEqual(-2, ephp:main(["test/code/file_not_found"])).
+    ?assertEqual(2, ephp:main(["test/code/file_not_found"])).
 
 main_error_test() ->
-    ?assertEqual(-1, ephp:main(["test/code/error.php"])).
+    ?assertEqual(1, ephp:main(["test/code/error.php"])).
 
 main_other_error_test() ->
-    ?assertEqual(-3, ephp:main(["test/"])).
+    ?assertEqual(3, ephp:main(["test/"])).
 
 main_empty_test() ->
-    ?assertEqual(-1, ephp:main([])).
+    ?assertEqual(1, ephp:main([])).
 
 register_var_test() ->
     {ok, Ctx} = ephp:context_new(),
