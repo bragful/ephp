cf85c80e3814d61332a1b026c6c7f3a1f7d94e8b
Manuel Rubio <manuel@altenwald.com>
2017-03-12 03:09:29 +0000

    tests for array merge and array union (+ operator)

---
+++ test/code/test_array_merge.php
@@ -0,0 +1,13 @@
+<?php
+$array1    = array("color" => "red", 2, 4);
+$array2    = array("a", "b", "color" => "green", "shape" => "trapezoid", 4);
+$resultado = array_merge($array1, $array2);
+print_r($resultado);
+
+$comienzo = 'foo';
+$fin = array(1 => 'bar');
+$resultado = array_merge((array)$comienzo, (array)$fin);
+print_r($resultado);
+
+var_dump(array_merge($comienzo, $fin));
+var_dump(array_merge([], [], $comienzo, $fin));
---
+++ test/code/test_array_union.php
@@ -0,0 +1,5 @@
+<?php
+$array1    = array(0 => 'zero_a', 2 => 'two_a', 3 => 'three_a');
+$array2    = array(1 => 'one_b', 3 => 'three_b', 4 => 'four_b');
+$resultado = $array1 + $array2;
+var_dump($resultado);
