b3e615d60da0382524bc2d27491ab06073b2c1b7
Manuel Rubio <manuel@altenwald.com>
2017-06-19 01:14:02 +0100

    add specs for ephp_array

--- src/ephp_array.erl
+++ src/ephp_array.erl
@@ -16,12 +16,20 @@
     to_list/1
 ]).
 
+-spec new() -> ephp_array().
+
 new() -> #ephp_array{}.
+
+-spec new(module(), function(), [any()]) -> ephp_array().
 
 new(Module, Function, Args) ->
     #ephp_array{trigger = {Module, Function, Args}}.
 
+-spec size(ephp_array()) -> non_neg_integer().
+
 size(#ephp_array{size=Size}) -> Size.
+
+-spec find(mixed(), ephp_array()) -> {ok, mixed()} | error.
 
 find(Key, #ephp_array{values=Values, trigger=undefined}) ->
     case lists:keyfind(Key, 1, Values) of
@@ -32,23 +40,25 @@ find(Key, #ephp_array{values=Values, trigger=undefined}) ->
 find(Key, #ephp_array{trigger={Module,Function,Args}}=Array) ->
     apply(Module, Function, Args ++ [Array, {retrieve, Key}]).
 
-store(auto, Value, #ephp_array{last_num_index=Key, values=Values}=Array) ->
+-spec store(auto | mixed(), mixed(), ephp_array()) -> ephp_array().
+
+store(auto, Value, #ephp_array{last_num_index = Key, values = Values} = Array) ->
     report(Array#ephp_array{
         last_num_index = Key + 1,
         values = Values ++ [{Key, Value}],
         size = Array#ephp_array.size + 1
     }, {add, Key, Value});
 
-store(Key, Value, #ephp_array{last_num_index=Last, values=Values}=Array) when
-        is_integer(Key) andalso Key >= 0 andalso
-        Last =< Key ->
+store(Key, Value, #ephp_array{last_num_index = Last, values = Values} = Array)
+        when is_integer(Key) andalso Key >= 0
+        andalso Last =< Key ->
     report(Array#ephp_array{
         last_num_index = Key,
         values = Values ++ [{Key, Value}],
         size = Array#ephp_array.size + 1
     }, {add, Key, Value});
 
-store(Key, Value, #ephp_array{values=Values}=Array) ->
+store(Key, Value, #ephp_array{values = Values} = Array) ->
     case lists:keyfind(Key, 1, Values) =/= false of
         true ->
             NewValues = lists:keyreplace(Key, 1, Values, {Key, Value}),
@@ -64,6 +74,8 @@ store(Key, Value, #ephp_array{values=Values}=Array) ->
         size = Size
     }, {Action, Key, Value}).
 
+-spec erase(mixed(), ephp_array()) -> ephp_array().
+
 erase(Key, #ephp_array{values=Values}=Array) ->
     NewValues = lists:keydelete(Key, 1, Values),
     report(Array#ephp_array{
@@ -71,12 +83,16 @@ erase(Key, #ephp_array{values=Values}=Array) ->
         size = length(NewValues)
     }, {remove, Key}).
 
+-spec fold(function(), mixed(), ephp_array()) -> mixed().
+
 fold(Fun, Initial, #ephp_array{values=Values, trigger=undefined}) ->
     lists:foldl(fun({K,V}, Acc) -> Fun(K, V, Acc) end, Initial, Values);
 
 fold(Fun, Initial, #ephp_array{trigger={Module,Function,Args}}=Array) ->
     NewFun = fun({K,V},Acc) -> Fun(K,V,Acc) end,
     apply(Module, Function, Args ++ [Array, {fold, NewFun, Initial}]).
+
+-spec from_list([mixed()]) -> ephp_array().
 
 from_list(List) when is_list(List) ->
     lists:foldl(fun
@@ -88,6 +104,8 @@ from_list(List) when is_list(List) ->
                          last_num_index = K + 1}
     end, #ephp_array{}, List).
 
+-spec to_list(ephp_array()) -> [mixed()].
+
 to_list(#ephp_array{values=Values, trigger=undefined}) ->
     Values;
 
@@ -98,8 +116,13 @@ to_list(#ephp_array{trigger={Module,Function,Args}}=Array) ->
 %% Internal functions
 %% -----------------------------------------------------------------------------
 
-report(#ephp_array{trigger=undefined}=Array, _) ->
+-type action() :: {add | update, Key :: mixed(), Value :: mixed()} |
+                  {remote, Key :: mixed()}.
+
+-spec report(ephp_array(), action()) -> ephp_array().
+
+report(#ephp_array{trigger = undefined} = Array, _) ->
     Array;
 
-report(#ephp_array{trigger={Module,Function,Args}}=Array, Action) ->
+report(#ephp_array{trigger = {Module, Function, Args}} = Array, Action) ->
     apply(Module, Function, Args ++ [Array, Action]).
